6926b35dcb0aa889d289c31c3734650053fd5495
Can Li <canl1@euler.cheme.cmu.edu>
2020-03-12 20:40:15 -0400

    update csv file

--- NSRDB_wind/gen_repn_days.jl
+++ NSRDB_wind/gen_repn_days.jl
@@ -4,7 +4,7 @@ for t=4:12
 	clust_res = run_clust(ts_input_data;method="kmeans",n_clust=t)
 	ts_clust_data = clust_res.clust_data
 	for (k,v) in ts_clust_data.data
-		a = DataFrame(v)
+		a = DataFrame(hcat(collect(1:24),v))
 		if !isdir(string(t,"days"))
 			mkdir(string(t,"days"))
 		end
