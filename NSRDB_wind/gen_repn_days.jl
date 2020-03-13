2c29f607ad2828c0c803729ad4b4e724b1ef9153
Can Li <canl1@euler.cheme.cmu.edu>
2020-03-12 20:07:31 -0400

    add CF and load data in NSRDB_wind

---
+++ NSRDB_wind/gen_repn_days.jl
@@ -0,0 +1,13 @@
+using TimeSeriesClustering, CSV, DataFrames
+ts_input_data = load_timeseries_data("/home/canl1/work/GETP/NSRDB_wind/for_cluster", T=24, years=[2012])
+for t=4:12
+	clust_res = run_clust(ts_input_data;method="kmeans",n_clust=t)
+	ts_clust_data = clust_res.clust_data
+	for (k,v) in ts_clust_data.data
+		a = DataFrame(v)
+		if !isdir(string(t,"days"))
+			mkdir(string(t,"days"))
+		end
+		CSV.write(string(t,"days/", k, ".csv"), a)
+	end
+end
